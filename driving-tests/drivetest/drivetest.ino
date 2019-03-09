adba06adab2456223d41f9883ead361f09f37eb7
ver09934 <ianvernooy@gmail.com>
2019-03-09 18:47:20 -0500

    Loop while bytes available instead of using readBytesUntil

--- driving-tests/drivetest/drivetest.ino
+++ driving-tests/drivetest/drivetest.ino
@@ -3,6 +3,9 @@
 Servo leftMotor;
 Servo rightMotor;
 
+int leftPower;
+int rightPower;
+
 byte myBuffer[2] = {0, 0};
 
 void setup() {
@@ -18,17 +21,20 @@ void setup() {
 
 void loop() {
 
-  if (Serial.available() > 0) {
-
-    Serial.readBytesUntil(0xff, myBuffer, 1000);
-
-    leftPower = map(myBuffer[0], 0, 0xff, 0, 180);
-    rightPower = map(myBuffer[1], 0, 0xff, 0, 180);
-
-    leftMotor.write(leftPower);
-    rightMotor.write(rightPower);
-
+  while (Serial.available() > 0) {
+    int inByte = Serial.read();
+    myBuffer[0] = myBuffer[1];
+    myBuffer[1] = inByte;
   }
+
+  leftPower = map(myBuffer[0], 0, 0xff, 0, 180);
+  rightPower = map(myBuffer[1], 0, 0xff, 0, 180);
+
+  leftMotor.write(leftPower);
+  rightMotor.write(rightPower);
+  
+  // Serial.println(myBuffer[0] + " " + myBuffer[1]);
+  // delay(500);
   delay(10);
 }
 
