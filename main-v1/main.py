25cfa823bbf6598c2b8199b7406afa19f0006b1a
ver09934 <ianvernooy@gmail.com>
2019-03-23 06:57:05 -0400

    Update demo to use new methods

--- main-v1/main.py
+++ main-v1/main.py
@@ -1,65 +1,21 @@
 import serialwriter
 import time
-import threading
-
-# TODO: This is inevitably going to need threading... so much fun!
-# TODO: Should probably try to document this process for the engineering notebook...
 
 def main():
-    serialWriter = serialwriter.SerialWriter()
+    
+    writer = serialwriter.SerialWriter()
     time.sleep(3)
-    print("Delay over")
-    # driveTest(serialWriter)
-    # stepperTest(serialWriter)
-    # relayTest(serialWriter)
-    demo(serialWriter)
 
-def driveTest(writer):
-    # writer.setLeftPower(1)
-    # writer.setRightPower(1)
-    writer.setLeftPower(0xff)
-    writer.setRightPower(0xff)
-    writer.writeAllBytes()
-    time.sleep(3)
-    # writer.setLeftPower(0.5)
-    # writer.setRightPower(0.5)
-    writer.setLeftPower(0x80)
-    writer.setRightPower(0x80)
-    writer.writeAllBytes()
-
-def stepperTest(writer):
-    writer.setStepperPosition(0xff)
-    writer.writeAllBytes()
-    time.sleep(21)
-    writer.setStepperPosition(0x00)
-    writer.writeAllBytes()
-
-def relayTest(writer):
-    # position, bit
-    # relay 1
-    writer.setBit(1, 1)
-    writer.writeAllBytes()
-    time.sleep(1)
-    writer.setBit(1, 0)
-    writer.writeAllBytes()
-    # relay 2
-    writer.setBit(2, 1)
-    writer.writeAllBytes()
-    time.sleep(1)
-    writer.setBit(2, 0)
-    writer.writeAllBytes()
-
-def demo(writer):
     turnTime = 3
 
-    writer.setLeftPower(0x00)
-    writer.setRightPower(0xff)
-    writer.setStepperPosition(0xff)
+    writer.setLeftPowerMapped(0)
+    writer.setRightPowerMapped(1)
+    writer.setStepperPositionMapped(1)
     writer.writeAllBytes()
     time.sleep(turnTime)
     
-    writer.setLeftPower(0x80)
-    writer.setRightPower(0x80)
+    writer.setLeftPowerMapped(0)
+    writer.setRightPowerMapped(0)
     writer.writeAllBytes()
     time.sleep(21 - turnTime)
 
@@ -71,21 +27,9 @@ def demo(writer):
     writer.writeAllBytes()
     time.sleep(0.5)
     
-    writer.setStepperPosition(0x00)
+    writer.setStepperPositionMapped(0)
     writer.writeAllBytes()
     time.sleep(21)
-
-'''
-def demo(writer):
-    threading.Thread(target=demo1, args=(writer,)).start()
-    threading.Thread(target=demo1, args=(writer,)).start()
-
-def demo1():
-    pass
-
-def demo2():
-    pass
-'''
 
 if __name__ == "__main__":
     main()
