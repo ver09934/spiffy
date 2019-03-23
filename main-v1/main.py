1cf6a170eecc164211eb154c0e8603a577da608c
ver09934 <ianvernooy@gmail.com>
2019-03-23 07:56:32 -0400

    Update demo time values and relay toggles

--- main-v1/main.py
+++ main-v1/main.py
@@ -1,15 +1,17 @@
 import serialwriter
 import time
 
+relay1 = False
+
 def main():
     
     writer = serialwriter.SerialWriter()
     time.sleep(3)
 
-    turnTime = 3
+    turnTime = 6
 
     writer.setLeftPowerMapped(0)
-    writer.setRightPowerMapped(1)
+    writer.setRightPowerMapped(0.4)
     writer.setStepperPositionMapped(1)
     writer.writeAllBytes()
     time.sleep(turnTime)
@@ -20,12 +22,20 @@ def main():
     time.sleep(21 - turnTime)
 
     time.sleep(0.5)
-    writer.setBit(1, 1)
+    writer.setBit(2, 1)
     writer.writeAllBytes()
     time.sleep(1)
-    writer.setBit(1, 0)
+    writer.setBit(2, 0)
     writer.writeAllBytes()
     time.sleep(0.5)
+
+    if relay1:
+        writer.setBit(1, 1)
+        writer.writeAllBytes()
+        time.sleep(1)
+        writer.setBit(1, 0)
+        writer.writeAllBytes()
+        time.sleep(0.5)
     
     writer.setStepperPositionMapped(0)
     writer.writeAllBytes()
