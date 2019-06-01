905ba37b05c01b8068f8557e11202648964920c6
ver09934 <ianvernooy@gmail.com>
2019-06-01 15:35:54 -0400

    Add loop timer, list markers to stdout

--- main/markertest.py
+++ main/markertest.py
@@ -7,14 +7,19 @@ import pandas as pd
 
 # pip install pillow opencv-python matplotlib pandas aruco
 
+import time
+
 cap = cv2.VideoCapture(2)
 
 while(True):
+
+    start_time = time.time()
     
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    # aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
     parameters =  aruco.DetectorParameters_create()
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
     frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
@@ -22,6 +27,13 @@ while(True):
     cv2.imshow('img', frame_markers)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    # print("looptime: {} s" .format(time.time() - start_time))
+
+    if ids is not None:
+        for id in ids:
+            print(id[0], end=' ')
+        print()
 
 cap.release()
 cv2.destroyAllWindows()
