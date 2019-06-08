0738acbb60ede5b177bfefb15648027fb6d8ccb4
ver09934 <ianvernooy@gmail.com>
2019-06-07 22:48:39 -0400

    Add somewhat bad startup and kill script

--- main/button-run.py
+++ main/button-run.py
@@ -1,24 +1,29 @@
 import time
 import RPi.GPIO as GPIO
+from subprocess import Popen
 
 button_pin = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-running = False
-
 button_lock = False
+
+p = None
 
 while True:
     if GPIO.input(button_pin) and not button_lock:
         button_lock = True
 
-        # TODO: Actually start and stop the other script
-        if not running:
-            pass
+        # Popen(['python3', 'cv-main-robot.py']).wait()
+
+        if p is None: # Never started
+            p = Popen(['python3', 'cv-main-robot.py'])
         else:
-            pass
+            if p.poll() is None: # Running
+                p.terminate()
+            else: # Not running
+                p = Popen(['python3', 'cv-main-robot.py'])
 
     else:
         button_lock = False        
