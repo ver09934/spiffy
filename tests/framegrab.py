b75ec246398e34247e5f7b49e48a9455bad638e3
ver09934 <ianvernooy@gmail.com>
2019-06-08 13:38:55 +0100

    Update framegrab test

--- tests/framegrab.py
+++ tests/framegrab.py
@@ -2,7 +2,9 @@ import cv2
 
 cap = cv2.VideoCapture(-1)
 
-ret, img = cap.read()
+for i in range(0, 100):
+    ret, img = cap.read()
+
 cv2.imwrite("/home/pi/test.jpg", img)
 
 # sudo python -m SimpleHTTPServer 80
